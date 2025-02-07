d0e34d55bc6f923881559d302a1b7e8481243290
Salvatore0609 <salvatoredesole1998@gmail.com>
2025-02-07 17:42:52 +0100

    m

--- Netflix-React/src/App.jsx
+++ Netflix-React/src/App.jsx
@@ -1,20 +1,20 @@
 import { Container } from "react-bootstrap";
 import "./App.css";
 import Footer from "./components/Footer";
-/* import Home from "./components/Home";  */
+import Home from "./components/Home";
 import TopBar from "./components/NavBar";
-import NetflixProfile from "./components/MyProfile";
+/* import NetflixProfile from "./components/MyProfile"; */
 /* import AccountSettings from "./components/Settings"; */
-/* import OrganizationArea from "./components/OrganizationArea"; */
+import OrganizationArea from "./components/OrganizationArea";
 
 function App() {
   return (
     <Container fluid className="p-0 bg-dark">
       <TopBar />
-      {/*   <OrganizationArea />
-      <Home /> */}
+      <OrganizationArea />
+      <Home />
       {/* <AccountSettings /> */}
-      <NetflixProfile />
+      {/* <NetflixProfile /> */}
       <Footer />
     </Container>
   );
