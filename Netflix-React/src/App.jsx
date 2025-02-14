c45e488f6d4de06de1bdd6839d6ad207301ba331
Salvatore0609 <salvatoredesole1998@gmail.com>
2025-02-14 11:12:41 +0100

    Update App.jsx

--- Netflix-React/src/App.jsx
+++ Netflix-React/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <TopBar />
       <OrganizationArea />
       <Home />
-      {/* <AccountSettings /> */}
+      {/* <AccountSettings />  */}
       {/* <NetflixProfile /> */}
       <Footer />
     </Container>
